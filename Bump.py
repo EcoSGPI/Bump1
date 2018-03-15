ee84d8011da9a2b21be87e272bd73d920c8ec380
Ent <abondi@entertainment.com>
2018-03-15 15:08:45 -0400

    syntax issue on self.bot

--- Bump.py
+++ Bump.py
@@ -5,17 +5,16 @@ from discord.ext import commands
 class Bump:
     """Bump servers!"""
 
-    def __init__(self, bot):
-        self.bot = bot
+def __init__(self, bot):
+    self.bot = bot
 
     @commands.command()
     async def bump(self):
         """Bumps servers!"""
 
-        #Your code will go here
-        await self.bot.say("=bump")
-	await self.bot.say("dlm!bump")
+#Your code will go here
+    await self.bot.say("=bump")
+    await self.bot.say("dlm!bump")
 
 def setup(bot):
     bot.add_cog(Bump(bot))
-
